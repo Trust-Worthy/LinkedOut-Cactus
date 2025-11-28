8588af37d29ad43a7a452e7f3f79632726988748
Trust-Worthy <jondeveloper0@gmail.com>
2025-11-28 13:43:41 -0700

    cleanign files.

--- README.md
+++
@@ -1,2 +0,0 @@
-# LinkedOut-Cactus
-LinkedOut is a contacts and networking app that leverages on device ML provided by LinkedOut
--- linkedout/android/app/src/main/kotlin/com/example/linkedout/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.linkedout
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
